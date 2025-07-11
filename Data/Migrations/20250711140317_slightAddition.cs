9d8eb121476bdd558188cc2c12938cd4ae7b1cb8
NET13_NguyenPhuocDuy <duynguyen4100@gmail.com>
2025-07-11 21:03:53 +0700

    update migration

---
+++ Data/Migrations/20250711140317_slightAddition.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class slightAddition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_AspNetUsers_UserId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Addresses",
+                type: "varchar(255)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_AspNetUsers_UserId",
+                table: "Addresses",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_AspNetUsers_UserId",
+                table: "Addresses");
+
+            migrationBuilder.UpdateData(
+                table: "Addresses",
+                keyColumn: "UserId",
+                keyValue: null,
+                column: "UserId",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Addresses",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_AspNetUsers_UserId",
+                table: "Addresses",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
